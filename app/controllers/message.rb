1ee4a286b8077ad9fad8cedaf094abbbbd53d32a
rhtaylor <randyisbusy@gmail.com>
2019-11-08 00:52:54 -0800

    updated action controller for robust protection of repeate emails and addition of Helper methods

--- app/controllers/message.rb
+++ app/controllers/message.rb
@@ -14,19 +14,19 @@ class MessageController <  Sinatra::Base
     
   post '/message/send' do  
     
-    @users_ids = params["user"]["id"] 
-    @users = @users_ids.map{ |id| User.find_by(id: id )}
-    @id = params["user"]["id"]
-    @packed_id = @id.join("-")
-    @new_params = {:message => params["message"], 
+      @users_ids = params["user"]["id"] 
+      @users = @users_ids.map{ |id| User.find_by(id: id )}
+      @packed_id = @users_ids.join("-") 
+      @new_params = {:message => params["message"], 
                    :author => params["author"],
                    :sent_to => @packed_id }
-    @message = Message.create(@new_params)
+      @message = Message.create(@new_params)
     
-    packed_ids = @message.sent_to 
-    ids = packed_ids.split("-") 
-    @sent_to_users = ids.map{ |x| User.find_by(id: x)} 
-    @user = User.find_by(username: @message.author)
+      packed_ids = @message.sent_to 
+      ids = packed_ids.split("-") 
+      @sent_to_users = ids.map{ |x| User.find_by(id: x)} 
+      @user = User.find_by(username: @message.author) 
+    
     erb :"/user/sent"
   end
     
@@ -45,16 +45,25 @@ class MessageController <  Sinatra::Base
      
    end
  
-   get '/message/:id' do 
-    binding.pry
-   end 
+   get '/message/:id' do  
+    id = params[:id]
+    @message = Message.find(params[:id]) 
+    ids = @message.sent_to.split("-")
+    @sent_to_users = ids.map{ |x| User.find_by(id: x)} 
+    
+    @user = User.find_by(username: @message.author)  
+    id = params[:id]
+    erb :"user/id"
+  end 
   
    delete '/message/:id/delete' do 
     
-    @message = Message.find(params["id"])
+    @message = Message.find(params["id"]) 
+    author = @message.author 
+    @user = User.find_by("author" ==  author)
     @message.destroy
+    id = @user.id
     
-    
-    redirect '/'
+    redirect "/user/#{id}"
   end  
 end
--- app/controllers/userc.rb
+++ app/controllers/userc.rb
@@ -11,21 +11,33 @@ class UserController <  Sinatra::Base
   
     get '/' do 
         session.clear
+        session["state"] = "reset"
+        @session = session
         erb :index 
     end 
     get '/user/signin' do
+        session[:page] = "signin"
         @session = session      
         erb :'user/signin'
        
     end  
-    get '/user/signup' do  
+    get '/user/signup' do
+      session[:page] = "signin"
+      @session = session  
     erb :'user/signup' 
 
     end 
     get "/user/logout" do
       session.clear
       erb :index
+    end 
+
+    get '/user/:id/sent_all' do  
+       @user = User.find_by(params)
+        @messages = Message.all.map{ |unit| unit.author == @user.username ? unit : next }  
+        erb :'/user/sent_all'
     end
+
        post '/user/signup' do 
     if params[:password_digest] == "" || params[:username] == "" || params[:email] == ""
          session["message"] = "try again" 
@@ -33,9 +45,15 @@ class UserController <  Sinatra::Base
          redirect "user/signup" 
     elsif User.find_by(params)  
          @user = User.find_by(params) 
+         session[:user_id] = @user.id
          redirect "user/#{@user.id}" 
-    else 
-         @user = User.create(params)
+    elsif User.find_by("email" == params[:email])
+          session["message"] = "email already used"
+          @session = session
+          redirect 'user/signup'
+    else
+         @user = User.create(params) 
+         session[:user_id] = @user.id
          redirect "user/#{@user.id}" 
       
     end
@@ -57,6 +75,6 @@ class UserController <  Sinatra::Base
       redirect "/user/#{@user.id}"
      end
      end
-     
+    
      
 end
