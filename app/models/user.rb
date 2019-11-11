89f29d758e99a60c57936492ab24a959a0394596
rhtaylor <randyisbusy@gmail.com>
2019-11-11 01:07:28 -0800

    validation and uniqueness added to signup process

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 require_relative '../../config/environment'
 class User < ActiveRecord::Base
+    validates :username, presence: true, uniqueness: true
     has_secure_password
     has_many :messages
 end
