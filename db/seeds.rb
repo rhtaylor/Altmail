82c54f3695449c54fcf760107e335e624671f94b
rhtaylor <randyisbusy@gmail.com>
2019-11-26 07:50:08 -0800

    updated seeds to have a password from password_digest in order to save to db

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+User.create(username: "Randy", email: "randyisbusy@gmail.com", password: "1234", password_confirmation: "1234") 
+User.create(username: "Candis", email: "ladyC@gmail.com", password: "idk", password_confirmation: "idk") 
+User.create(username: "Hackerman", email: "crypt.tor", password: "Hackerman", password_confirmation: "Hackerman")
