10557a6e3941cda478110432f331c0d03c265b46
rhtaylor <randyisbusy@gmail.com>
2019-11-26 13:04:10 -0800

    updated get http requests to conceal app information

--- app/controllers/blog.rb
+++ app/controllers/blog.rb
@@ -2,7 +2,7 @@ require_relative '../../config/environment'
 require_relative './userc'
 class BlogController < UserController
     get '/blog' do 
-        @messages = Message.all.sort{ |x,y| x.created_at <=> y.created_at }
+        @messages = params.has_key?("filtered") ?  Message.all.sort{ |x,y| y.created_at <=> x.created_at } : Message.all.sort{ |x,y| x.created_at <=> y.created_at }
         @all = User.all 
         erb :'/blog/all'
     end
